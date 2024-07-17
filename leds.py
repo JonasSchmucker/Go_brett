d79e0ba8dbd416df04e9d60d5c0513d120931b69
Jonas Schmucker <jonas@schmucker.net>
2024-07-17 20:52:50 +0200

    set correct pin numbers

--- leds.py
+++ leds.py
@@ -1,18 +1,18 @@
 import gpio
 
-__GPIO_PIN_MUL_LED_IN_ADDRESS_START__ = 0
-__GPIO_PIN_MUL_LED_OUT_ADDRESS_START__ = 0
+# __GPIO_PIN_MUL_STONES_IN_ADDRESS__ = [13, 14, 15, 16]
+# __GPIO_PIN_MUL_STONES_OUT_ADDRESS__ = [1, 2, 3, 5]
 
-__GPIO_PIN_EXTRA_LED_IN_1__ = 0
-__GPIO_PIN_EXTRA_LED_IN_2__ = 0
-__GPIO_PIN_EXTRA_LED_IN_3__ = 0
+__GPIO_PIN_MUL_EN_1_IN__ = 17
+__GPIO_PIN_MUL_EN_2_IN__ = 18
+__GPIO_PIN_MUL_EN_1_OUT__ = 6
+__GPIO_PIN_MUL_EN_2_OUT__ = 7
+__GPIO_PIN_MUL_LEDS_IN_ADDRESS__ = [13, 14, 15, 16]
+__GPIO_PIN_MUL_LEDS_OUT_ADDRESS__ = [1, 2, 3, 5]
+__GPIO_MUL_ADDRESS_SIZE__ = 4
+__GPIO_MUL_MAX_ADDRESS__= 16
+__GPIO_BOARD_SIZE__ = 19
 
-__GPIO_PIN_EXTRA_LED_OUT_1__ = 0
-__GPIO_PIN_EXTRA_LED_OUT_2__ = 0
-__GPIO_PIN_EXTRA_LED_OUT_3__ = 0
-
-__GPIO_PIN_MUL_LED_OUT_ENABLE__ = 0
-__GPIO_PIN_MUL_LED_IN_ENABLE__ = 0
 
 current_x = -1
 current_y = -1
@@ -21,92 +21,90 @@ def set_led(x: int, y: int):
     return
 
 
-
-def write_address_in(address: int):
-    if address >= 16 or address < 0:
-        print("Invalid address" + str(address))
+def write_address(address: int, direction_out: bool):
+    if address >= __GPIO_BOARD_SIZE__ or address < 0:
+        logging.critical("Invalid address" + str(address))
         return
-    
-    if address >= 8:
-        address -= 8
-        gpio.set_pin_high(__GPIO_PIN_MUL_LED_IN_ADDRESS_START__)
+    direction_string = ""
+    multiplexer_string = ""
+    if address >= __GPIO_MUL_MAX_ADDRESS__:
+        multiplexer_string = "2"
+        address -= __GPIO_MUL_MAX_ADDRESS__
+        if direction_out:
+            direction_string = "OUT"
+            gpio.set_pin_high(__GPIO_PIN_MUL_EN_1_OUT__)
+            gpio.set_pin_low(__GPIO_PIN_MUL_EN_2_OUT__)
+        else:
+            direction_string = "IN"
+            gpio.set_pin_high(__GPIO_PIN_MUL_EN_1_IN__)
+            gpio.set_pin_low(__GPIO_PIN_MUL_EN_2_IN__)
     else:
-        gpio.set_pin_low(__GPIO_PIN_MUL_LED_IN_ADDRESS_START__)
-
-    if address >= 4:
-        address -= 4
-        gpio.set_pin_high(__GPIO_PIN_MUL_LED_IN_ADDRESS_START__ + 1)
-    else:
-        gpio.set_pin_low(__GPIO_PIN_MUL_LED_IN_ADDRESS_START__ + 1)
-
-    if address >= 2:
-        address -= 2
-        gpio.set_pin_high(__GPIO_PIN_MUL_LED_IN_ADDRESS_START__ + 2)
-    else:
-        gpio.set_pin_low(__GPIO_PIN_MUL_LED_IN_ADDRESS_START__ + 2)
-
-    if address >= 1:
-        address -= 1
-        gpio.set_pin_high(__GPIO_PIN_MUL_LED_IN_ADDRESS_START__ + 3)
-    else:
-        gpio.set_pin_low(__GPIO_PIN_MUL_LED_IN_ADDRESS_START__ + 3)
-
-def write_address_out(address: int):
-    if address >= 16 or address < 0:
-        print("Invalid address" + str(address))
-        return
-    
-    if address >= 8:
-        address -= 8
-        gpio.set_pin_high(__GPIO_PIN_MUL_LED_OUT_ADDRESS_START__)
-    else:
-        gpio.set_pin_low(__GPIO_PIN_MUL_LED_OUT_ADDRESS_START__)
-
-    if address >= 4:
-        address -= 4
-        gpio.set_pin_high(__GPIO_PIN_MUL_LED_OUT_ADDRESS_START__ + 1)
-    else:
-        gpio.set_pin_low(__GPIO_PIN_MUL_LED_OUT_ADDRESS_START__ + 1)
-
-    if address >= 2:
-        address -= 2
-        gpio.set_pin_high(__GPIO_PIN_MUL_LED_OUT_ADDRESS_START__ + 2)
-    else:
-        gpio.set_pin_low(__GPIO_PIN_MUL_LED_OUT_ADDRESS_START__ + 2)
-
-    if address >= 1:
-        address -= 1
-        gpio.set_pin_high(__GPIO_PIN_MUL_LED_OUT_ADDRESS_START__ + 3)
-    else:
-        gpio.set_pin_low(__GPIO_PIN_MUL_LED_OUT_ADDRESS_START__ + 3)
+        multiplexer_string = "1"
+        if direction_out:
+            direction_string = "OUT"
+            gpio.set_pin_low(__GPIO_PIN_MUL_EN_1_OUT__)
+            gpio.set_pin_high(__GPIO_PIN_MUL_EN_2_OUT__)
+        else:
+            direction_string = "IN"
+            gpio.set_pin_low(__GPIO_PIN_MUL_EN_1_IN__)
+            gpio.set_pin_high(__GPIO_PIN_MUL_EN_2_IN__)
+    logging.info("Writing Address " 
+                 + direction_string + " to " 
+                 + str(address) + ", using Multiplexer " 
+                 + multiplexer_string)
+    write_address_8(address, direction_out)
 
 
-def init_leds():
-    gpio.set_pin_as_output(__GPIO_PIN_MUL_LED_OUT_ENABLE__)
-    gpio.set_pin_as_output(__GPIO_PIN_MUL_LED_IN_ENABLE__)
-    
-    gpio.set_pin_low(__GPIO_PIN_MUL_LED_OUT_ENABLE__)
-    gpio.set_pin_low(__GPIO_PIN_MUL_LED_OUT_ENABLE__)
+def write_address_8(address: int, direction_out: bool):
+    address_array = __GPIO_PIN_MUL_STONES_OUT_ADDRESS__ if direction_out else __GPIO_PIN_MUL_STONES_IN_ADDRESS__
+    current_address_bit = 1
+    for i in range(__GPIO_MUL_ADDRESS_SIZE__):
+        set_to = "error"
+        if address & current_address_bit:
+            set_to = "high"
+            gpio.set_pin_high(address_array[i]) # counting from zero
+        else:
+            set_to = "low"
+            gpio.set_pin_low(address_array[i]) # counting from zero
+            
+        logging.debug("Setting Pin with adress index " + str(i) 
+                    + ", linear ID " + str(address_array[i])
+                    + ", GPIO ID " + str(gpio.get_inverted_mapped_value(address_array[i]))
+                    + " to " + set_to)
+        current_address_bit = current_address_bit << 1
 
-    for i in range(4):
-        gpio.set_pin_as_output(__GPIO_PIN_MUL_LED_IN_ADDRESS_START__ + i)
-        gpio.set_pin_as_output(__GPIO_PIN_MUL_LED_OUT_ADDRESS_START__ + i)
 
-    write_address_in(0)
-    write_address_out(0)
+def init_stones():
+    gpio.gpio_init_mode()
 
-    gpio.set_pin_as_output(__GPIO_PIN_EXTRA_LED_IN_1__)
-    gpio.set_pin_as_output(__GPIO_PIN_EXTRA_LED_IN_2__)
-    gpio.set_pin_as_output(__GPIO_PIN_EXTRA_LED_IN_3__)
+    for i in range(__GPIO_MUL_ADDRESS_SIZE__):
+        gpio.set_pin_as_output(__GPIO_PIN_MUL_STONES_IN_ADDRESS__[i])
+        gpio.set_pin_as_output(__GPIO_PIN_MUL_STONES_OUT_ADDRESS__[i])
 
-    gpio.set_pin_low(__GPIO_PIN_EXTRA_LED_IN_1__)
-    gpio.set_pin_low(__GPIO_PIN_EXTRA_LED_IN_2__)
-    gpio.set_pin_low(__GPIO_PIN_EXTRA_LED_IN_3__)
+    gpio.set_pin_as_output(__GPIO_PIN_MUL_EN_1_IN__)
+    gpio.set_pin_as_output(__GPIO_PIN_MUL_EN_2_IN__)
+    gpio.set_pin_as_output(__GPIO_PIN_MUL_EN_1_OUT__)
+    gpio.set_pin_as_output(__GPIO_PIN_MUL_EN_2_OUT__)
+    gpio.set_pin_as_input(__GPIO_PIN_STONES_OUT__)
 
-    gpio.set_pin_as_output(__GPIO_PIN_EXTRA_LED_OUT_1__)
-    gpio.set_pin_as_output(__GPIO_PIN_EXTRA_LED_OUT_2__)
-    gpio.set_pin_as_output(__GPIO_PIN_EXTRA_LED_OUT_3__)
+    write_address(0, True)
+    write_address(0, False)
+    logging.warning("Finished initialising stones")
 
-    gpio.set_pin_low(__GPIO_PIN_EXTRA_LED_OUT_1__)
-    gpio.set_pin_low(__GPIO_PIN_EXTRA_LED_OUT_2__)
-    gpio.set_pin_low(__GPIO_PIN_EXTRA_LED_OUT_3__)
+
+def get_stones() -> list[(int, int)]:
+    stones = list()
+    for x in range(__GPIO_BOARD_SIZE__):
+        for y in range(__GPIO_BOARD_SIZE__):
+            if get_stone(x, y):
+                stones += [(x, y)]
+    return stones
+
+def get_stone(x: int, y: int) -> bool:
+    write_address(x, True) # Out
+    write_address(y, False) # In
+    time.sleep(__READ_DELAY_SECONDS__)
+    is_stone = gpio.read_pin(__GPIO_PIN_STONES_OUT__)
+    stone_string = "" if is_stone else "NO "
+    logging.info("Detected " + stone_string + " stone at x=" + str(x) + ", y=" + str(y))
+    return 
